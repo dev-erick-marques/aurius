91d91fe7265015a15477fcc6864929982a07f2b4
Erick Marques <erickmarques2564@gmail.com>
2026-06-07 20:10:12 -0300

    starting aurius project

---
+++ src/main/java/com/aurius/AuriusApplication.java
@@ -0,0 +1,13 @@
+package com.aurius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuriusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuriusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aurius/AuriusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuriusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
